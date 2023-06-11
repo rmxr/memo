cd06266d25f6937e06800ba0a6663848facf0375
Anton Voytsekhovskiy <rmxr@yandex.ru>
2023-06-11 11:54:01 +0300

    fix: animation bugs

--- src/components/PlayField/PlayField.tsx
+++ src/components/PlayField/PlayField.tsx
@@ -21,9 +21,9 @@ function PlayField() {
 
     if (openCardOne.imageUrl === openCardTwo.imageUrl) {
       setSwitchedCardIds(switchedCardIds.concat(openCardIds));
+    } else {
+      setOpenCardIds([]);
     }
-
-    setOpenCardIds([]);
   }, [openCardIds, switchedCardIds]);
 
   React.useEffect(() => {
