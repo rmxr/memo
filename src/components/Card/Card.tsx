bf3247c6662882f3ec46f6beedd65aa2887a2791
Anton Voytsekhovskiy <rmxr@yandex.ru>
2023-06-18 14:11:43 +0300

    revert to useEffect

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,6 @@
 import styles from "./Card.module.css";
 import back from "../../assets/back.jpeg";
 import React from "react";
-
 function Card({
   imageUrl,
   off,
@@ -14,72 +13,40 @@ function Card({
   onClick: () => void;
 }) {
   const cardholderRef = React.useRef<HTMLDivElement>(null);
-  const [entryPoint, setEntryPoint] = React.useState({
-    offsetX: 0,
-    offsetY: 0,
-  });
 
-  // React.useEffect(() => {
-  //   if (!cardholderRef.current) {
-  //     return;
-  //   }
-  //   const element = cardholderRef.current;
-  //   let entryPoint = { offsetX: 0, offsetY: 0 };
-  //   function track(event: MouseEvent) {
-  //     element.style.transform = `translate(${
-  //       (event.offsetX - entryPoint.offsetX) * 0.02
-  //     }px, ${(event.offsetY - entryPoint.offsetY) * 0.02}px)`;
-  //   }
-  //
-  //   function startTracking(event: MouseEvent) {
-  //     entryPoint = { offsetX: event.offsetX, offsetY: event.offsetY };
-  //     element.addEventListener("mousemove", track);
-  //   }
-  //
-  //   function stopTracking() {
-  //     element.removeEventListener("mousemove", track);
-  //     element.removeAttribute("style");
-  //   }
-  //
-  //   element.addEventListener("mouseenter", startTracking);
-  //   element.addEventListener("mouseleave", stopTracking);
-  //
-  //   return () => {
-  //     element.removeEventListener("mouseenter", startTracking);
-  //     element.removeEventListener("mouseleave", stopTracking);
-  //   };
-  // }, [cardholderRef]);
+  React.useEffect(() => {
+    if (!cardholderRef.current) {
+      return;
+    }
+    const element = cardholderRef.current;
+    let entryPoint = { offsetX: 0, offsetY: 0 };
+    function track(event: MouseEvent) {
+      element.style.transform = `translate(${
+        (event.offsetX - entryPoint.offsetX) * 0.02
+      }px, ${(event.offsetY - entryPoint.offsetY) * 0.02}px)`;
+    }
 
-  const handleMouseMove: React.MouseEventHandler<HTMLDivElement> = (event) => {
-    const el = event.target as HTMLDivElement;
-    const offsetX = (event.nativeEvent.offsetX - entryPoint.offsetX) * 0.02;
-    const offsetY = (event.nativeEvent.offsetY - entryPoint.offsetY) * 0.02;
+    function startTracking(event: MouseEvent) {
+      entryPoint = { offsetX: event.offsetX, offsetY: event.offsetY };
+      element.addEventListener("mousemove", track);
+    }
 
-    el.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-  };
+    function stopTracking() {
+      element.removeEventListener("mousemove", track);
+      element.removeAttribute("style");
+    }
 
-  const handleMouseLeave: React.MouseEventHandler<HTMLDivElement> = (event) => {
-    const el = event.target as HTMLDivElement;
+    element.addEventListener("mouseenter", startTracking);
+    element.addEventListener("mouseleave", stopTracking);
 
-    el.removeAttribute("style");
-  };
-
-  const handleMouseEnter: React.MouseEventHandler<HTMLDivElement> = (event) => {
-    setEntryPoint({
-      offsetX: event.nativeEvent.offsetX,
-      offsetY: event.nativeEvent.offsetY,
-    });
-  };
+    return function cleanup() {
+      element.removeEventListener("mouseenter", startTracking);
+      element.removeEventListener("mouseleave", stopTracking);
+    };
+  }, [cardholderRef]);
 
   return (
-    <div
-      className={styles.cardholder}
-      ref={cardholderRef}
-      onClick={onClick}
-      onMouseEnter={handleMouseEnter}
-      onMouseMove={handleMouseMove}
-      onMouseLeave={handleMouseLeave}
-    >
+    <div className={styles.cardholder} ref={cardholderRef} onClick={onClick}>
       <div
         className={`${styles.card} ${flip ? styles.flipped : ""} ${
           off ? `${styles.off} ${styles.flipped}` : ""
