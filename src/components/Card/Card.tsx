b6bd47b9cf527e09eea9d4cebaf6341d2e94aa82
Anton Voytsekhovskiy <rmxr@yandex.ru>
2023-06-22 14:41:04 +0400

    feature: implement spring physics

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,8 @@
 import styles from "./Card.module.css";
 import back from "../../assets/back.jpeg";
 import React from "react";
+import { useSpring, animated } from "@react-spring/web";
+
 function Card({
   imageUrl,
   off,
@@ -13,6 +15,8 @@ function Card({
   onClick: () => void;
 }) {
   const cardholderRef = React.useRef<HTMLDivElement>(null);
+  let xOffset = 0;
+  let yOffset = 0;
 
   React.useEffect(() => {
     if (!cardholderRef.current) {
@@ -33,7 +37,6 @@ function Card({
 
     function stopTracking() {
       element.removeEventListener("mousemove", track);
-      element.removeAttribute("style");
     }
 
     element.addEventListener("mouseenter", startTracking);
@@ -45,8 +48,58 @@ function Card({
     };
   }, [cardholderRef]);
 
+  const [springs, api] = useSpring(
+    () => ({
+      x: 0,
+      y: 0,
+      onStart: () => {
+        console.log(xOffset, yOffset);
+      },
+      config: {
+        mass: 2,
+        friction: 14,
+        tension: 120,
+      },
+    }),
+    []
+  );
+
+  const handleMouseLeave = () => {
+    const regex = /translate\(([-\d.]+)px,\s*([-\d.]+)px\)/;
+
+    if (cardholderRef.current) {
+      const matches = regex.exec(cardholderRef.current.style.cssText);
+
+      if (matches) {
+        xOffset = parseFloat(matches[1]);
+        yOffset = parseFloat(matches[2]);
+      }
+    }
+    api.start({
+      from: {
+        x: xOffset,
+        y: yOffset,
+      },
+      to: {
+        x: 0,
+        y: 0,
+      },
+    });
+  };
+
+  const handleMouseEnter = () => {
+    api.stop();
+  };
+
   return (
-    <div className={styles.cardholder} ref={cardholderRef} onClick={onClick}>
+    <animated.div
+      className={styles.cardholder}
+      ref={cardholderRef}
+      onClick={onClick}
+      onMouseLeave={handleMouseLeave}
+      onMouseEnter={handleMouseEnter}
+      style={{ ...springs }}
+    >
       <div
         className={`${styles.card} ${flip ? styles.flipped : ""} ${
           off ? `${styles.off} ${styles.flipped}` : ""
@@ -59,7 +112,7 @@ function Card({
           <img className={styles.image} src={back} alt={"Card Back"} />
         </div>
       </div>
-    </div>
+    </animated.div>
   );
 }
 
